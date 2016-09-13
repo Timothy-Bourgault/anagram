fd3ddd059b7390f229dde5b24ae14b22f80beeb1
Seth Kendall and Tim Bourgault <seth@>
2016-09-13 13:07:20 -0700

    update business logic

--- src/Anagram.php
+++ src/Anagram.php
@@ -4,17 +4,28 @@
         function detectAnagram($word1, $word2, $word3, $word4, $word5)
         {
             $test_array = array($word1, $word2, $word3, $word4, $word5);
-            $keyword = array_shift($test_array);
             $test_array2 = array();
+            $win_words = array();
             foreach ($test_array as $word) {
                 $working_word = str_split($word);
                 sort($working_word);
                 $working_word2 = implode(",", $working_word);
                 array_push($test_array2, $working_word2);
             }
-            foreach ($test_array as $anagram) {
-
+            $keyword = array_shift($test_array2);
+            for ($i=0; $i <count($test_array2); $i++) {
+                if ($test_array2[$i]==$keyword){
+                    array_push($win_words,$test_array[$i+1]);
+                }
             }
+            // foreach ($test_array2 as $anagram) {
+            //     $counter = 0;
+            //     $counter++;
+            //     if ($anagram==$keyword){
+            //         array_push($win_words,$test_array[$counter]);
+            //     }
+            //
+            // }
             // $word_array = array($word1, $word2, $word3, $word4, $word5);
             // $array1 = array();
             // $array2 = array();
@@ -33,7 +44,7 @@
             //     // }
             //     // $counter++;
             // }
-             return $test_array2;
+             return $win_words;
 
         }
     }
